2a53e1b1d6b326965d263e5dbb2fdde9860206ac
Moni <cam60228@gmail.com>
2026-09-03 22:22:21 +0530

    updated

---
+++ javascrpit/Day12/task/assets/js/index.js
@@ -0,0 +1,25 @@
+
+//push()
+
+const numbers=[1,2,3,4,5]
+numbers.push(6,7,8)
+console.log("final array",numbers);
+
+//pop()
+let fruits=["strawberry","mango","dragon","apple","custard apple","kiwi"]
+fruits.pop()
+fruits.pop()
+console.log("final array",fruits);
+console.log();
+
+
+console.log("Create an array of 5 city names. Remove the first city using shift(), then add a new city at the beginning using unshift().");
+
+
+let city=["nagai","chennai","bangalore","mumbai","madurai"]
+city.shift()
+console.log(city);
+city.unshift()
+console.log(city);
+
+
