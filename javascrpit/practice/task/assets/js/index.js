0740272b948cb102d30a16d0fcb3050c797b2903
Moni <cam60228@gmail.com>
2026-09-07 17:08:25 +0530

    updated

---
+++ javascrpit/practice/task/assets/js/index.js
@@ -0,0 +1,6 @@
+const title=document.getElementId("title")
+const btn=document.getElementId("btn")
+btn.addEventlistener("CLICK",()=>{
+    showbtn.textcontent="blue";
+    showbtn.backgroundcolor="red";
+});
